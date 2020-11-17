a4e4534f965322a0cb64ae616321d6a3a96ef832
unknown <ayelet98k@gmail.com>
2020-11-17 15:34:29 +0200

    fix database

--- config.py
+++ config.py
@@ -1,4 +1,20 @@
-TELEGRAM_TOKEN ='1438436987:AAG5n5E1h9sXKeoT7zrbcWOBO__dkvuqfCg'
-WEBHOOK_URL = 'https://1bbc57d8b4d5.ngrok.io'
-TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}/message'.format(TELEGRAM_TOKEN, WEBHOOK_URL)
+import pymysql
+
+
+# config bot
 WEBHOOK_PORT = 5002
+TELEGRAM_TOKEN ='1438436987:AAG5n5E1h9sXKeoT7zrbcWOBO__dkvuqfCg'
+WEBHOOK_URL = 'https://3f75f7e5b6fa.ngrok.io'
+TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}/message'.format(TELEGRAM_TOKEN, WEBHOOK_URL)
+
+
+# config database
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db="covidbotdb",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
--- dbHandler.py
+++ dbHandler.py
@@ -1,12 +1,62 @@
-
-
+from config import connection
 from person import *
 from location import *
 
 
 def insert_person(person):
-    pass
+    with connection.cursor() as cursor:
+        query = f"insert into person values ({person.person_id}, {person.name}, {person.phone}, {person.user_name}, {person.state});"
+        cursor.execute(query)
+        connection.commit()
 
 
 def insert_location(location):
-    pass
+    with connection.cursor() as cursor:
+        query = f"insert into location values ({location.lat}, {location.lon});"
+        cursor.execute(query)
+        connection.commit()
+
+
+def insert_location_person(person_location):
+    with connection.cursor() as cursor:
+        query = f"insert into LocationPerson values ({person_location.datetime_start}, {person_location.duration}, " \
+                f"{person_location.is_mask}" \
+                f"{person_location.is_open_space},{person_location.userName},{person_location.lat},{person_location.lon});"
+        cursor.execute(query)
+        connection.commit()
+
+
+def is_red_location(lat, lon, date):
+    with connection.cursor() as cursor:
+        query = f"select * from LocationPerson where lat = {lat} and lon = {lon} and startDateTime.date = {date};"
+        cursor.execute(query)
+        res = cursor.fetchall()
+        if res[0]:
+            return True
+        return False
+
+
+def get_date_time_and_duration(lat, lon, date):
+    with connection.cursor() as cursor:
+        query = f"select * from LocationPerson where lat = {lat} and lon = {lon};"
+        cursor.execute(query)
+        res = cursor.fetchall()
+        return res[0]
+
+
+def mok_db():
+    with connection.cursor() as cursor:
+        query = "insert into person values(209311181, 'aya', 025375858, '0987', 0);"
+        cursor.execute(query)
+        connection.commit()
+        query = "insert into location values('31.75165995', '35.18739009689732');"
+        cursor.execute(query)
+        connection.commit()
+        query = "insert into LocationPerson values('2020-11-16 10:30:00', 75, 1, 0, 0987, '31.75165995', '35.18739009689732');"
+        cursor.execute(query)
+        connection.commit()
+
+
+#mok_db()
+
+print(is_red_location('31.75165995', '35.18739009689732', '2020-11-16'))
