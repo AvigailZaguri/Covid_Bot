8dbe12f29fd9f8a16524a1287588466a9e8ea7ef
Yael-Rubin <yael8477@gmail.com>
2020-11-18 22:13:53 +0200

    flow_refactor

--- dbHandler.py
+++ dbHandler.py
@@ -36,6 +36,7 @@ def set_id_by_user_name(user_name, id):
         cursor.execute(query)
         connection.commit()
 
+
 def insert_day_daignose(user_name, day_daignose):
     with connection.cursor() as cursor:
         query = f"update person set day_daignose = '{day_daignose}'" \
@@ -69,7 +70,15 @@ def get_location(location):
 
 def insert_location_person(person_location):
     with connection.cursor() as cursor:
-        query =f"insert into LocationPerson values ('{person_location.datetime_start}',{person_location.duration},1,1,'{person_location.person_id}','{person_location.lat}','{person_location.lon}');"
+        query = f"insert into LocationPerson values ('{person_location.datetime_start}',{person_location.duration},1,1,'{person_location.person_id}','{person_location.lat}','{person_location.lon}');"
+        cursor.execute(query)
+        connection.commit()
+
+
+def set_duration_by_user_name(user_name, duration):
+    with connection.cursor() as cursor:
+        query = f"update LocationPerson set duration = '{duration}'" \
+                f"where telegramUserName = '{user_name}';"
         cursor.execute(query)
         connection.commit()
 
@@ -116,10 +125,8 @@ def set_state_by_user_name(user_name, state):
 def get_location_by_name_and_time(location, time):
     with connection.cursor() as cursor:
         query = f"select lat,lon from locationperson where lat='{location.lat}' and lon='{location.lon}'" \
-            f" and '{time}' >= time(startDateTime)" \
-            f" and  '{time}' <= time(startDateTime)+ interval duration minute;"
+                f" and '{time}' >= time(startDateTime)" \
+                f" and  '{time}' <= time(startDateTime)+ interval duration minute;"
         cursor.execute(query)
         location = cursor.fetchone()
         return location
-
-
